f13a1c53351d5f14d82238362fb7e1b727b6c02f
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-01-28 00:27:21 +0100

    created a css for the home page and added a hero section image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import NavJ from "../components/NavJ";
-
+import "../pages/home.css";
 const QuestionDashboard = () => {
   return (
     <div>
       <NavJ />
-      <div className="container pt-5">
-        <h1>welcome to the homepage</h1>
+      <div>
+        <div className="section custom-section ">
+          <div className="overlay">
+            <h1>welcome to JBQ</h1>
+          </div>
+        </div>
       </div>
     </div>
   );
