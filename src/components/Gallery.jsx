94f0a761f811ce8cdf32f5c74bb43442dab11bea
Philemon Okpokpa <remotephil.upwork@gmail.com>
2024-01-29 05:51:49 +0100

    created gallery section

---
+++ src/components/Gallery.jsx
@@ -0,0 +1,161 @@
+import React from "react";
+import "../components/gallery.css";
+
+const Gallery = () => {
+  return (
+    <div>
+      <div className="gallery container text-center mt-5">
+        <h2 className="mt-5">Benefits of Participation</h2>
+        <div className="gallery-item d-flex mt-5 align-items-center">
+          <div className="description w-100 d-flex flex-column justify-content-center align-items-center">
+            <h4>Networking</h4>
+            <p className="w-50 mt-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+              voluptas impedit dolor eos eveniet repudiandae quasi iste tenetur
+              quam earum ex laboriosam accusantium debitis esse reiciendis,
+              repellat dignissimos fugit quidem ipsa. Labore assumenda error
+              debitis consequuntur sunt id reprehenderit enim, non dignissimos
+              libero obcaecati sit blanditiis possimus distinctio voluptas
+              recusandae!
+            </p>
+          </div>
+          <div className="picture w-100">
+            <img
+              className="w-100 greyscale"
+              src="src/assets/sharedhappiness.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="gallery-item d-flex flex-row-reverse align-items-center">
+          <div className="description w-100 d-flex flex-column justify-content-center align-items-center">
+            <h4>Bible Knowledge</h4>
+            <p className="w-50 mt-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+              voluptas impedit dolor eos eveniet repudiandae quasi iste tenetur
+              quam earum ex laboriosam accusantium debitis esse reiciendis,
+              repellat dignissimos fugit quidem ipsa. Labore assumenda error
+              debitis consequuntur sunt id reprehenderit enim, non dignissimos
+              libero obcaecati sit blanditiis possimus distinctio voluptas
+              recusandae!
+            </p>
+          </div>
+          <div className="picture w-100">
+            <img
+              className="w-100 greyscale"
+              src="src/assets/Photo_1656184765077.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="gallery-item d-flex  align-items-center">
+          <div className="description w-100 d-flex flex-column justify-content-center align-items-center">
+            <h4>Critical thinking</h4>
+            <p className="w-50  mt-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+              voluptas impedit dolor eos eveniet repudiandae quasi iste tenetur
+              quam earum ex laboriosam accusantium debitis esse reiciendis,
+              repellat dignissimos fugit quidem ipsa. Labore assumenda error
+              debitis consequuntur sunt id reprehenderit enim, non dignissimos
+              libero obcaecati sit blanditiis possimus distinctio voluptas
+              recusandae!
+            </p>
+          </div>
+          <div className="picture w-100">
+            <img
+              className="w-100 greyscale"
+              src="src/assets/criticalthinking.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="gallery-item d-flex flex-row-reverse  align-items-center">
+          <div className="description w-100 d-flex flex-column justify-content-center align-items-center">
+            <h4>Memory Improvement</h4>
+            <p className="w-50 mt-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+              voluptas impedit dolor eos eveniet repudiandae quasi iste tenetur
+              quam earum ex laboriosam accusantium debitis esse reiciendis,
+              repellat dignissimos fugit quidem ipsa. Labore assumenda error
+              debitis consequuntur sunt id reprehenderit enim, non dignissimos
+              libero obcaecati sit blanditiis possimus distinctio voluptas
+              recusandae!
+            </p>
+          </div>
+          <div className="picture w-100">
+            <img
+              className="w-100 greyscale"
+              src="src/assets/memoryboost.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="gallery-item d-flex  align-items-center">
+          <div className="description w-100 d-flex flex-column justify-content-center align-items-center">
+            <h4>Self-Confidence Boost</h4>
+            <p className="w-50 mt-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+              voluptas impedit dolor eos eveniet repudiandae quasi iste tenetur
+              quam earum ex laboriosam accusantium debitis esse reiciendis,
+              repellat dignissimos fugit quidem ipsa. Labore assumenda error
+              debitis consequuntur sunt id reprehenderit enim, non dignissimos
+              libero obcaecati sit blanditiis possimus distinctio voluptas
+              recusandae!
+            </p>
+          </div>
+          <div className="picture w-100">
+            <img
+              className="w-100 greyscale"
+              src="src/assets/selfconfidenceboost.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="gallery-item d-flex flex-row-reverse  align-items-center">
+          <div className="description w-100 d-flex flex-column justify-content-center align-items-center">
+            <h4>Teamwork and Collaboration</h4>
+            <p className="w-50 mt-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+              voluptas impedit dolor eos eveniet repudiandae quasi iste tenetur
+              quam earum ex laboriosam accusantium debitis esse reiciendis,
+              repellat dignissimos fugit quidem ipsa. Labore assumenda error
+              debitis consequuntur sunt id reprehenderit enim, non dignissimos
+              libero obcaecati sit blanditiis possimus distinctio voluptas
+              recusandae!
+            </p>
+          </div>
+          <div className="picture w-100">
+            <img
+              className="w-100 greyscale"
+              src="src/assets/teamwork.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="gallery-item d-flex  align-items-center">
+          <div className="description w-100 d-flex flex-column justify-content-center align-items-center">
+            <h4>Spirituality</h4>
+            <p className="w-50 mt-5">
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni
+              voluptas impedit dolor eos eveniet repudiandae quasi iste tenetur
+              quam earum ex laboriosam accusantium debitis esse reiciendis,
+              repellat dignissimos fugit quidem ipsa. Labore assumenda error
+              debitis consequuntur sunt id reprehenderit enim, non dignissimos
+              libero obcaecati sit blanditiis possimus distinctio voluptas
+              recusandae!
+            </p>
+          </div>
+          <div className="picture w-100">
+            <img
+              className="w-100 greyscale"
+              src="src/assets/Photo_1656184761991.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
